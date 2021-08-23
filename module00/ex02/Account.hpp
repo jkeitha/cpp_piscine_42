9f3112b4331eeff24f4805006d65b1cf3ef71e40
nicolas75m <nhciwehgo3247@gmail.com>
2021-08-23 15:51:06 +0300

    module00 ex02 upd

--- module00/ex02/Account.hpp
+++ module00/ex02/Account.hpp
@@ -8,10 +8,6 @@ public:
 
 	typedef Account		t;
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
 	static void	displayAccountsInfos( void );
 
 	Account( int initial_deposit );
@@ -19,7 +15,6 @@ public:
 
 	void	makeDeposit( int deposit );
 	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
 	void	displayStatus( void ) const;
 
 
