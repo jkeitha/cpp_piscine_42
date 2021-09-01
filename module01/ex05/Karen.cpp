42109712fe6bee1931cab952ad3fbf8fa87f66b3
nicolas75m <nhciwehgo3247@gmail.com>
2021-09-01 17:14:02 +0300

    module01 ex05 upd: I don't understand how to make a switch with string

--- module01/ex05/Karen.cpp
+++ module01/ex05/Karen.cpp
@@ -2,36 +2,53 @@
 
 void	Karen::debug( void )
 {
-	std::cout << "I love to get extra bacon for my \
-	7XL-double-cheese-triple-pickle-special-ketchup \
-	burger. I just love it!\n";
+	std::cout << "DEBUG:\t\t"
+	"I love to get extra bacon for my "
+	"7XL-double-cheese-triple-pickle-special-ketchup "
+	"burger. I just love it!\n";
 }
 
 void	Karen::info( void )
 {
-	std::cout << "I cannot believe adding extra bacon \
-	cost more money. You don’t put enough! If you did \
-	I would not have to ask for it!\n";
+	std::cout << "INFO:\t\t"
+	"I cannot believe adding extra bacon "
+	"cost more money. You don’t put enough! If you did "
+	"I would not have to ask for it!\n";
 }
 
 void	Karen::warning( void )
 {
-	std::cout << "I think I deserve to have some extra \
-	bacon for free. I’ve been coming here for years and \
-	you just started working here last month.\n";
+	std::cout << "WARNING:\t"
+	"I think I deserve to have some extra "
+	"bacon for free. I’ve been coming here for years and "
+	"you just started working here last month.\n";
 }
 
 void	Karen::error( void )
 {
-	std::cout << "This is unacceptable, I want to speak \
-	to the manager now.\n";
+	std::cout << "ERROR:\t\t"
+	"This is unacceptable, I want to speak "
+	"to the manager now.\n";
 }
 
 void	Karen::complain( std::string level )
 {
-	(void)level;
-	// void	(Karen::*debug_ponter) ();
-	// debug_ponter = &Karen::debug;
+	/*
+	void	(Karen::*debug_ponter) ();
+	debug_ponter = &Karen::debug;
+	*/
+
 	void	(Karen::*debug_ponter) () = &Karen::debug;
-	(this->*debug_ponter) ();
+	void	(Karen::*info_ponter) () = &Karen::info;
+	void	(Karen::*warning_ponter) () = &Karen::warning;
+	void	(Karen::*error_ponter) () = &Karen::error;
+
+	if (level == "DEBUG")
+		(this->*debug_ponter) ();
+	if (level == "INFO")
+		(this->*info_ponter) ();
+	if (level == "WARNING")
+		(this->*warning_ponter) ();
+	if (level == "ERROR")
+		(this->*error_ponter) ();
 }
