c9f62f610c6107de6f062ff085a927aadcefc582
Self Principal <sprincip@il-i3.msk.21-school.ru>
2021-09-10 14:23:08 +0300

    module02 ex00 upd

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -1,1 +1,25 @@
 #include "Fixed.hpp"
+
+/*
+**	getter and setter
+*/
+
+int		Fixed::getRawBits(void) const { return m_point; }
+
+void	Fixed::setRawBits(int const raw) { m_point = raw; }
+
+/*
+**	assignation operator overload
+*/
+
+Fixed	&Fixed::operator=(const Fixed &v) {}
+
+/*
+**	constructors and destructor
+*/
+
+Fixed::Fixed(const Fixed &v) {}
+
+Fixed::Fixed() { m_point = 0; }
+
+Fixed::~Fixed() {}
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -3,8 +3,19 @@
 
 class	Fixed
 {
+	private:
+
+	int			m_point;
+	const int	m_bit = 8;
+
 	public:
 
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+	Fixed	&operator=(const Fixed &v);
+
+	Fixed(const Fixed &v);
 	Fixed();
 	~Fixed();
 };
