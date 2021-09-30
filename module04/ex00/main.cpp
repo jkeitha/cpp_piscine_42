9afdca33b2f7c6215c885c7c894fca1e28a0d0c3
nicolas75m <nhciwehgo3247@gmail.com>
2021-09-30 17:07:17 +0300

    module04 ex00 upd

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -1,4 +1,23 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
 int	main()
 {
+	const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+	std::cout << "Animal getType: " << meta->getType() << "\n";
+	std::cout << "Dog getType: " << j->getType() << std::endl;
+	std::cout << "Cat getType: " << i->getType() << std::endl;
+
+	std::cout << "Animal makeSound: "; meta->makeSound();
+	std::cout << "Dog makeSound: "; j->makeSound();
+	std::cout << "Cat makeSound: "; i->makeSound(); // will output the cat sound!
+
+	delete meta;
+	delete j;
+	delete i;
+
 	return 0;
 }
